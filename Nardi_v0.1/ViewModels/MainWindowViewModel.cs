27ebe9981e0063deffe675cb3f733e9af20e1489
Aleksey <alendk.real@gmail.com>
2023-05-15 08:10:38 +0300

    Update

---
+++ Nardi_v0.1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nardi_v0._1.ViewModels.Base
+{
+    internal class MainWindowViewModel : ViewModel
+
+    {
+        #region WindowTitle
+
+        private string _Title = "Нарды v0.1";
+        /// <summary> WindowTitle </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+    }
+
+}
